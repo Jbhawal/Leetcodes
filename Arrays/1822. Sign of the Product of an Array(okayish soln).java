0af93b897760062347613c07067ca8872b991a1f
Sbhawal <mailtosnehaan@gmail.com>
2025-03-16 23:55:40 +0530

    Update Leetcode solution on 2024 -Thu 08-15

---
+++ Arrays/1822. Sign of the Product of an Array(okayish soln).java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public int arraySign(int[] nums) {
+        int n= nums.length;
+        int sign=1;
+        for(int i=0; i<n; i++){
+            if(nums[i]<0) sign = - sign;
+            else if (nums[i]==0) return 0;
+        } 
+        return sign;   
+    }
+}
