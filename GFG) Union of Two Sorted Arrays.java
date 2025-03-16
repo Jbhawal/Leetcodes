44d32ae0a86ca4738750aa28536787fd0223ed94
Sbhawal <mailtosnehaan@gmail.com>
2025-03-16 23:53:56 +0530

    Update GFG solutions on 2024 -Sun 06-23

--- GFG) Union of Two Sorted Arrays.java
+++ GFG) Union of Two Sorted Arrays.java
@@ -1,43 +1,30 @@
 import java.util.*;
-
-class TUF{
-static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
-   int i = 0, j = 0; // pointers
-  ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
-  while (i < n && j < m) {
-    if (arr1[i] <= arr2[j]) // Case 1 and 2
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
     {
-      if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
-        Union.add(arr1[i]);
-      i++;
-    } else // case 3
-    {
-      if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
-        Union.add(arr2[j]);
-      j++;
+        // add your code here
+        ArrayList<Integer>Union = new ArrayList<>();
+        int i=0, j=0;
+        while (i<n && j<m){
+            if(arr1[i]<=arr2[j]){
+                if(Union.size()==0 || Union.get(Union.size()-1)!=arr1[i]) Union.add(arr1[i]);
+                i++;
+            }
+            else{
+                if(Union.size()==0 || Union.get(Union.size()-1)!= arr2[j]) Union.add(arr2[j]);
+                j++;
+            }
+        }
+        while(i<n){
+            if(Union.get(Union.size()-1)!=arr1[i]) Union.add(arr1[i]);
+            i++;
+        }
+        while(j<m){
+            if(Union.get(Union.size()-1)!=arr2[j]) Union.add(arr2[j]);
+            j++;
+        }
+        return Union;
     }
-  }
-  while (i < n) // IF any element left in arr1
-  {
-    if (Union.get(Union.size()-1) != arr1[i])
-      Union.add(arr1[i]);
-    i++;
-  }
-  while (j < m) // If any elements left in arr2
-  {
-    if (Union.get(Union.size()-1) != arr2[j])
-      Union.add(arr2[j]);
-    j++;
-  }
-  return Union;
-}
-public static void main(String args[]) {
-  int n = 11, m = 7;
-  int arr1[] = { 4, 5,5, 5, 5, 5, 6, 7, 8, 10, 11};
-  int arr2[] = {2, 3, 4, 14, 5, 11, 12};
-  ArrayList<Integer> Union = FindUnion(arr1, arr2, n, m);
-  System.out.println("Union of arr1 and arr2 is ");
-  for (int val: Union)
-    System.out.print(val+" ");
-}
 }
--- pushLeetcodes.bat
+++ pushLeetcodes.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d/JB/Leet codes
+cd /d/JB/Leetcodes
 
 REM Get the current date in YYYY-MM-DD format
 for /f "tokens=1-3 delims=/" %%a in ('date /t') do (
@@ -10,7 +10,7 @@ for /f "tokens=1-3 delims=/" %%a in ('date /t') do (
 set "today=%YYYY%-%MM%-%DD%"
 
 git add .
-git commit -m "Update LeetCode solutions on %today%"
+git commit -m "Update GFG solutions on %today%"
 git push origin main
 
 echo LeetCode solutions pushed to GitHub successfully!
