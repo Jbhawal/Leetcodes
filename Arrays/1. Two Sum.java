7e54263c90c885267cfb24404aee55bfcc5b872a
Sbhawal <mailtosnehaan@gmail.com>
2025-03-16 23:55:52 +0530

    Update Leetcode solution on 2025 -Sat 02-15

--- Arrays/1. Two Sum.java
+++ Arrays/1. Two Sum.java
@@ -1,14 +1,17 @@
+import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int rem = target - nums[i];
-            if (map.containsKey(rem)) {
-                return new int[] { map.get(rem), i };
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n= nums.length;
+        for(int i=0; i<n; i++){
+            int rem=target- nums[i];
+            if (map.containsKey(rem)){
+                return new int[]{i, map.get(rem)};
             }
-            map.put(nums[i], i);
-        }    
-    return new int[] {};
+            else{
+                map.put(nums[i], i);
+            }
+        }
+        return new int[] {};
     }
 }
-
