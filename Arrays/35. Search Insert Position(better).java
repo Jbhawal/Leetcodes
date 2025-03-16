ccd6da61fc2850887bce7f8241f0d120b0b6dda9
Sbhawal <mailtosnehaan@gmail.com>
2025-03-16 23:56:02 +0530

    Update Leetcode solution on 2025 -Sun 03-16

--- Arrays/35. Search Insert Position(better).java
+++ Arrays/35. Search Insert Position(better).java
@@ -1,3 +1,6 @@
+//okay solution with O(n) time complexity
+//optimal has O(logn) time complexity
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n= nums.length;
