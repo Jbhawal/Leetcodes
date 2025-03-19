a5467f09182663c79bc7577d8c53bab767a58386
Sbhawal <joyitabhawal@gmail.com>
2025-03-19 21:08:13 +0530

    Updated Leetcode solution on 2025-03-19

--- pushLeetcodes.bat
+++ pushLeetcodes.bat
@@ -18,7 +18,6 @@
 
 
 @echo off
-@REM cd /d JB\Leetcodes
 cd /d D:\JB\Leetcodes
 
 REM Extract date in YYYY-MM-DD format using WMIC (works consistently)
@@ -26,7 +25,7 @@ for /f %%i in ('wmic os get localdatetime ^| find "."') do set datetime=%%i
 set today=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
 
 git add .
-git commit -m "Update Leetcode solution on %today%"
+git commit -m "Updated Leetcode solution on %today%"
 git pull origin main --rebase
 git push origin main
 
