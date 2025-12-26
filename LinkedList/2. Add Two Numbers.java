6d48ca55bc193882f39e5d0e6c8012682907c301
Jbhawal <joyitabhawal@gmail.com>
2025-12-26 15:31:27 +0530

    Updated Leetcode solution on 2025-12-26

---
+++ LinkedList/2. Add Two Numbers.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry=0, sum=0;
+        ListNode curr1=l1;
+        ListNode curr2=l2;
+        ListNode l3 = new ListNode(0);
+        ListNode curr3=l3;
+        while(curr1!=null || curr2!=null || carry!=0){
+            sum= carry;
+            if(curr1!=null){
+                sum+= curr1.val;
+                curr1=curr1.next;
+            }
+            if(curr2!=null){
+                sum+= curr2.val;
+                curr2=curr2.next;
+            }
+            curr3.next=new ListNode(sum%10);
+            curr3= curr3.next;
+            carry=sum/10;
+        }
+    return l3.next;
+    }
+}
